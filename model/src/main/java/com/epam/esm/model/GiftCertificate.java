0f35fcc03b7f5b60e25c3e4e87b97d1254f1e41e
Vladislav Kuzmich <kyzmenoid@gmail.com>
2021-11-30 09:35:31 +0300

    add model files

---
+++ model/src/main/java/com/epam/esm/model/GiftCertificate.java
@@ -0,0 +1,22 @@
+package com.epam.esm.model;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+public class GiftCertificate {
+    int id;
+    String name;
+    String description;
+    BigDecimal price;
+    int duration;
+    Date createDate;
+    Date lastUpdateDate;
+}
---
+++ model/src/main/java/com/epam/esm/model/Tag.java
@@ -0,0 +1,14 @@
+package com.epam.esm.model;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+public class Tag {
+    int id;
+    String name;
+}
