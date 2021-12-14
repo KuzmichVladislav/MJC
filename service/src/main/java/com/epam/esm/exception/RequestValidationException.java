f7b6f327d775d1230049a9d597851bfc2ec5a70b
Vladislav Kuzmich <kyzmenoid@gmail.com>
2021-12-14 12:21:30 +0300

    fixed my exceptions

--- service/src/main/java/com/epam/esm/exception/RequestValidationException.java
+++ service/src/main/java/com/epam/esm/exception/RequestValidationException.java
@@ -6,23 +6,6 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public class RequestValidationException extends RuntimeException {
-
     private String messageKey;
     private String messageParameter;
-
-    public RequestValidationException() {
-        super();
-    }
-
-    public RequestValidationException(String message) {
-        super(message);
-    }
-
-    public RequestValidationException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public RequestValidationException(Throwable cause) {
-        super(cause);
-    }
 }
--- service/src/main/java/com/epam/esm/exception/ResourceNotFoundException.java
+++ service/src/main/java/com/epam/esm/exception/ResourceNotFoundException.java
@@ -6,23 +6,6 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public class ResourceNotFoundException extends RuntimeException {
-
     private String messageKey;
     private String messageParameter;
-
-    public ResourceNotFoundException() {
-        super();
-    }
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-
-    public ResourceNotFoundException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public ResourceNotFoundException(Throwable cause) {
-        super(cause);
-    }
 }
